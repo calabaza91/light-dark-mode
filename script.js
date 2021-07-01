895c9054d868c41559203f2dfe879df20e58b964
Caleb Cox <calebcox2010@gmail.com>
2021-06-30 22:41:11 -0500

    Add local storage
    This is currently broken

--- script.js
+++ script.js
@@ -38,13 +38,26 @@ function switchTheme(event) {
     if (event.target.checked) {
         // If checked, Dark Mode
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         // Else, Light Mode
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
